6329c5731adcce044c2d041ad95d280a1aa93ce6
curyAilen <cury.ailena@gmail.com>
2022-06-11 14:40:29 -0300

    avances

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ app.use('/imagenes', express.static(__dirname + '/public/images'))
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/home.html')
 })
+app.get('/register', function(req, res) {
+    res.sendFile(__dirname + '/views/register.html')
+})
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/views/login.html')
+})
 
 
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log("Servidor Corriendo")
 })
